a0452d883d7e8dbd57fac53f15694962c10078ad
wangdongdong <wangdongdong@ainirobot.com>
2018-01-05 17:17:56 +0800

    增加kotlin-android-extensions扩展依赖，button进行测试，使用anko的toast扩展

--- app/src/main/java/com/mazouri/solo/kotlin/MainActivity.kt
+++ app/src/main/java/com/mazouri/solo/kotlin/MainActivity.kt
@@ -2,11 +2,20 @@ package com.mazouri.solo.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnClick(v:View) {
+        toast("U click the button")
     }
 }
